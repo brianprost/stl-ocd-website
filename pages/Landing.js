fbf50c023209523ffa371bf074845341f724595f
Brian Prost <brian.s.prost@gmail.com>
2021-12-28 16:37:52 -0500

    landing page with logo

--- pages/Landing.js
+++ pages/Landing.js
@@ -1,18 +1,23 @@
+import Image from 'next/image'
+import heroPic from '../public/img/stl-ocd-group-therapy-hero.svg'
+
 export default () => {
   return (
     <div className="bg-white-100">
-      <div className="container mx-auto flex flex-col items-center py-12 sm:py-24">
-        <div className="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10">
-          <h1 className="text-2xl sm:text-8xl md:text-6xl lg:text-7xl xl:text-8xl text-left text-gray-800 font-title leading-7 md:leading-10">
-            Helping Hands for Those With
-            <span className="text-blue-700"> OCD </span>
-          </h1>
-          <p className="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg"></p>
+      <div className="container flex px-8 py-4 mx-auto md:flex lg:flex lg:py-48 xl:flex 2xl:flex">
+        <div className="flex flex-col justify-evenly w-full lg:flex-row">
+          <div className="max-w-lg md:w-full">
+            <h1 className="text-8xl md:text-8xl lg:text-7xl xl:text-8xl tracking-wide text-center lg:text-left md:text-center text-gray-900 font-title md:leading-2">
+              Helping Hands for Those With
+              <span className="text-blue-700"> OCD </span>
+            </h1>
+            <button className="w-1/2 flex justify-center py-2 px-4 rounded-md shadow-lg text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-blue-800 font-bold m-8">
+              Upcoming Events
+            </button>
+          </div>
         </div>
-        <div className="flex justify-center items-center">
-          <button className="ml-4 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 bg-transparent transition duration-150 ease-in-out hover:border-blue-600 lg:text-xl lg:font-bold  hover:text-blue-600 rounded border border-blue-700 text-blue-700 px-4 sm:px-10 py-2 sm:py-4 text-sm">
-            See Upcoming Events
-          </button>
+        <div className="md:flex items-center justify-center w-full h-96">
+          <Image src={heroPic} alt="" className="object-cover w-full h-full max-w-2xl rounded-md" />
         </div>
       </div>
     </div>
