071ef2fb6dc427723c4a76c9bec7b0ff2ec8f87f
Brian Prost <bprost94@gmail.com>
2021-12-29 12:12:20 -0500

    books page now has table of book data

--- pages/Books/data.js
+++ pages/Books/data.js
@@ -1,30 +1,22 @@
 export const Books = [
     {
         "title": "Blink, Blink, Clop, Clop: Why Do We Do Things We Can’t Stop? An OCD Storybook",
-        "author(s)": "E. Katia Moritz, Jennifer Jablonsky",
-        "ISBN-10": "1882732723",
-        "ISBN-13": "978-1882732722",
-        "asin": "",
+        "author": "E. Katia Moritz, Jennifer Jablonsky",
+        "isbn": "978-1882732722",
     },
     {
         "title": "Freeing Your Child from Obsessive-Compulsive Disorder",
-        "author(s)": "Tamar E. Chansky",
-        "ISBN-10": "812931173",
-        "ISBN-13": "978-0812931174",
-        "asin": "",
+        "author": "Tamar E. Chansky",
+        "isbn": "978-0812931174",
     },
     {
         "title": "Kids Like Me: Children’s Stories About Obsessive Compulsive Disorder",
-        "author(s)": "Constance H. Foster",
-        "ISBN-10": "",
-        "ISBN-13": "",
-        "asin": "B000K0F9V0",
+        "author": "Constance H. Foster",
+        "isbn": "(ASIN) B000K0F9V0",
     },
     {
         "title": "Living With Children: New Methods for Parents and Teachers",
-        "author(s)": "Gerald Roy Patterson",
-        "ISBN-10": "878221301",
-        "ISBN-13": "978-0878221301",
-        "asin": "",
+        "author": "Gerald Roy Patterson",
+        "isbn": "978-0878221301",
     }
   ]
---
+++ pages/Books/index.js
@@ -0,0 +1,48 @@
+import Navbar from '../Navbar'
+import { Books } from './data'
+
+export default () => {
+    return (
+        <>
+            <Navbar />
+            <div class="flex flex-col">
+                <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="overflow-hidden">
+                            <table class="min-w-full border text-center">
+                                <thead class="border-b">
+                                    <tr>
+                                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                                            Title
+                                        </th>
+                                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                                            Author
+                                        </th>
+                                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 border-r">
+                                            ISBN
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {Books.map((book) => (
+                                        <tr class="border-b">
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">
+                                                {book.title}
+                                            </td>
+                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                                                {book.author}
+                                            </td>
+                                            <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
+                                                {book.isbn}
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
