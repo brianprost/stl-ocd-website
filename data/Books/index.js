517d9c9894ff4df867d9c4c23057d89ab7e859b2
Brian Prost <brian.s.prost@gmail.com>
2022-01-07 12:50:06 -0500

    books page now available

--- data/Books/index.js
+++ data/Books/index.js
@@ -1,45 +1,53 @@
 export const Books = [
   {
     "Authors": "E. Katia Moritz; Jennifer Jablonsky",
-    "ISBN-13": "978-1882732722 ",
+    "ISBN13": "978-1882732722 ",
     "Title":
       "Blink, Blink, Clop, Clop: Why Do We Do Things We Can't Stop? An OCD Storybook",
+      "bookCover": "TBD"
   },
   {
     "Authors": "Tamar E. Chansky",
-    "ISBN-13": "978-0812931174 ",
+    "ISBN13": "978-0812931174 ",
     "Title": "Freeing Your Child from Obsessive-Compulsive Disorder",
+    "bookCover": "TBD"
   },
   {
     "Authors": "Constance H. Foster",
-    "ISBN-13": "9780963907035",
+    "ISBN13": "9780963907035",
     "Title":
       "Kids Like Me: Children's Stories About Obsessive Compulsive Disorder",
+      "bookCover": "TBD"
   },
   {
     "Authors": "Gerald Roy Patterson",
-    "ISBN-13": "978-0878221301",
+    "ISBN13": "978-0878221301",
     "Title": "Living With Children: New Methods for Parents and Teachers",
+    "bookCover": "TBD"
   },
   {
     "Authors": "John S. March, MD MPH; Karen Mulle Friesen ",
-    "ISBN-13": "978-1572302426",
+    "ISBN13": "978-1572302426",
     "Title":
       "OCD in Children and Adolescents: A Cognitive-Behavioral Treatment Manual",
+      "bookCover": "TBD"
   },
   {
     "Authors": "Aureen Pinto Wagner",
-    "ISBN-13": "978-0967734736 ",
+    "ISBN13": "978-0967734736 ",
     "Title": "Worried No More: Help and Hope for Anxious Children",
+    "bookCover": "TBD"
   },
   {
     "Authors": "Rena Benson; Jose Arturo",
-    "ISBN-13": "978-0963907059",
+    "ISBN13": "978-0963907059",
     "Title": "You do that too? Adolescents and OCD",
+    "bookCover": "TBD"
   },
   {
     "Authors": "Rod Fadem",
-    "ISBN-13": "978-0974290058",
+    "ISBN13": "978-0974290058",
     "Title": "Memoirs of a Born Shlepper:  Never Give OCD a Third Thought",
+    "bookCover": "TBD"
   },
 ];
--- pages/books/index.jsx
+++ pages/books/index.jsx
@@ -1,49 +1,23 @@
-import Navbar from '../../components/Navbar'
-import { Books } from '../../data/Books'
+import Navbar from "../../components/Navbar";
+import { Books } from "../../data/Books";
 
 export default () => {
-    return (
-        <>
-            <Navbar />
-            <div className="flex flex-col">
-                <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div className="flex justify-center items-center py-2 min-w-full sm:px-6 lg:px-8">
-                        <h1 className="text-center text-4xl font-title text-blue-700">Books</h1>
-                        <div className="container overflow-hidden">
-                            <table className="min-w-full border text-center mt-40">
-                                <thead className="border-b bg-neutral-500 ">
-                                    <tr>
-                                        <th scope="col" className="text-sm font-bold text-neutral-50 px-6 py-4 border-r">
-                                            Title
-                                        </th>
-                                        <th scope="col" className="text-sm font-bold text-neutral-50 px-6 py-4 border-r">
-                                            Author
-                                        </th>
-                                        <th scope="col" className="text-sm font-bold text-neutral-50 px-6 py-4 border-r">
-                                            ISBN
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {Books.map((book) => (
-                                        <tr className="border-b">
-                                            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 border-r">
-                                                {book.Title}
-                                            </td>
-                                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
-                                                {book.Authors}
-                                            </td>
-                                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap border-r">
-                                                {book['ISBN-13']}
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div class="antialiased container w-full md:px-4 mx-auto py-20">
+        <h1 className="text-6xl lg:text-7xl text-center font-title text-blue-800 my-10 drop-shadow-md">
+          Books
+        </h1>
+        <div class="md:grid lg:grid-cols-3 md:grid-cols-2 mlg:grid-cols-3 md:gap-10 space-y-6 md:space-y-0 px-10 md:px-0 mx-auto">
+          {Books.map((book) => (
+            <div class="bg-neutral-200 px-6 py-10 shadow-md rounded-md flex flex-col h-full justify-between my-4 md:my-auto">
+                  <h3 class="font-bold text-center mb-6 text-3xl tracking-normal text-black select-all">
+                    {book.Title}
+                  </h3>
+              <p class="mb-2">Author(s): {book.Authors}</p>
+              <p class="mb-2 select-none">ISBN-13: <span className="select-all">{book.ISBN13}</span></p>
             </div>
-        </>
-    )
-}
+          ))}
+        </div>
+      </div>
+  );
+};
