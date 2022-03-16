573efcc974bb5ec6bd92fdc29b2f8a0d451fb476
Brian Prost <brian.s.prost@gmail.com>
2022-03-16 15:33:13 -0700

    navbar hover styling change

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <header className="w-full object-cover mb-1">
-      <nav className="bg-white w-full">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="container flex flex-wrap items-center justify-between h-20">
-            <div className="h-3/4 md:h-full relative">
+    <header className="mb-1 w-full object-cover">
+      <nav className="w-full bg-white">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="container flex h-20 flex-wrap items-center justify-between">
+            <div className="relative h-3/4 md:h-full">
               <Link href="/">
                 <a>
                   <img
@@ -22,25 +22,25 @@ const Navbar = () => {
               </Link>
             </div>
             <div className="hidden w-full md:block md:w-auto">
-              <div className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 tracking-widest">
+              <div className="mt-4 flex flex-col tracking-wider md:mt-0 md:flex-row md:space-x-8">
                 <Link href="/#home">
-                  <a className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 hover:scale-125">
+                  <a className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700">
                     Home
                   </a>
                 </Link>
 
                 <Link href="/#about">
-                  <a className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 hover:scale-125">
+                  <a className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700">
                     About
                   </a>
                 </Link>
                 <Link href="/#information">
-                  <a className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 hover:scale-125">
+                  <a className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700">
                     Information
                   </a>
                 </Link>
                 <Link href="/#contact">
-                  <a className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 hover:scale-125">
+                  <a className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700">
                     Contact
                   </a>
                 </Link>
@@ -51,7 +51,7 @@ const Navbar = () => {
               <button
                 onClick={() => setIsOpen(!isOpen)}
                 type="button"
-                className="bg-neutral-100 inline-flex items-center justify-center p-2 rounded-md text-blue-700 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white border-2 border-black"
+                className="inline-flex items-center justify-center rounded-md border-2 border-black bg-neutral-100 p-2 text-blue-700 hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
               >
@@ -105,10 +105,10 @@ const Navbar = () => {
         >
           {(ref) => (
             <div className="md:hidden" id="mobile-menu">
-              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              <div ref={ref} className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
                 <Link href="/#home">
                   <a
-                    className="bg-neutral-100 text-blue-700 hover:bg-blue-100 border border-blue-800 text-2xl block px-3 py-6 rounded-3xl text-center font-sans"
+                    className="block rounded-3xl border border-blue-800 bg-neutral-100 px-3 py-6 text-center font-sans text-2xl text-blue-700 hover:bg-blue-100"
                     onClick={() => setIsOpen(!isOpen)}
                   >
                     <a>Home</a>
@@ -116,7 +116,7 @@ const Navbar = () => {
                 </Link>
                 <Link href="/#about">
                   <a
-                    className="bg-neutral-100 text-blue-700 hover:bg-blue-100 border border-blue-800 text-2xl block px-3 py-6 rounded-3xl text-center font-sans"
+                    className="block rounded-3xl border border-blue-800 bg-neutral-100 px-3 py-6 text-center font-sans text-2xl text-blue-700 hover:bg-blue-100"
                     onClick={() => setIsOpen(!isOpen)}
                   >
                     <a>About</a>
@@ -124,7 +124,7 @@ const Navbar = () => {
                 </Link>
                 <Link href="/#information">
                   <a
-                    className="bg-neutral-100 text-blue-700 hover:bg-blue-100 border border-blue-800 text-2xl block px-3 py-6 rounded-3xl text-center font-sans"
+                    className="block rounded-3xl border border-blue-800 bg-neutral-100 px-3 py-6 text-center font-sans text-2xl text-blue-700 hover:bg-blue-100"
                     onClick={() => setIsOpen(!isOpen)}
                   >
                     <a>Information</a>
@@ -132,7 +132,7 @@ const Navbar = () => {
                 </Link>
                 <Link href="/#contact">
                   <a
-                    className="bg-neutral-100 text-blue-700 hover:bg-blue-100 border border-blue-800 text-2xl block px-3 py-6 rounded-3xl text-center font-sans"
+                    className="block rounded-3xl border border-blue-800 bg-neutral-100 px-3 py-6 text-center font-sans text-2xl text-blue-700 hover:bg-blue-100"
                     onClick={() => setIsOpen(!isOpen)}
                   >
                     <a>Contact</a>
@@ -145,6 +145,6 @@ const Navbar = () => {
       </nav>
     </header>
   );
-}
+};
 
 export default Navbar;
