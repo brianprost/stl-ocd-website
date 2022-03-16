4f8cd367c5f4670f873f044dbeb4d8640a660101
Brian Prost <brian.s.prost@gmail.com>
2022-03-16 14:28:46 -0700

    added book cover images

--- data/Books.js
+++ data/Books.js
@@ -1,53 +1,52 @@
 export const Books = [
   {
-    "Authors": "E. Katia Moritz; Jennifer Jablonsky",
-    "ISBN13": "978-1882732722 ",
-    "Title":
-      "Blink, Blink, Clop, Clop: Why Do We Do Things We Can't Stop? An OCD Storybook",
-      "bookCover": "TBD"
+    Author: ["E. Katia Moritz", "Jennifer Jablonsky"],
+    ISBN13: "978-1882732722",
+    Title: "Blink, Blink, Clop, Clop: An OCD Storybook",
+    bookCover: "./img/book-covers/blink-blink-clop-clop-an-ocd-storybook.jpeg",
   },
   {
-    "Authors": "Tamar E. Chansky",
-    "ISBN13": "978-0812931174 ",
-    "Title": "Freeing Your Child from Obsessive-Compulsive Disorder",
-    "bookCover": "TBD"
+    Author: ["Tamar E. Chansky"],
+    ISBN13: "978-0812931174",
+    Title: "Freeing Your Child from Obsessive-Compulsive Disorder",
+    bookCover: "./img/book-covers/freeing-your-child-from-obsessive-compulsive-disorder.jpeg",
   },
   {
-    "Authors": "Constance H. Foster",
-    "ISBN13": "9780963907035",
-    "Title":
+    Author: ["Constance H. Foster"],
+    ISBN13: "9780963907035",
+    Title:
       "Kids Like Me: Children's Stories About Obsessive Compulsive Disorder",
-      "bookCover": "TBD"
+    bookCover: "./img/book-covers/kids-like-me-children-s-stories-about-obsessive-compulsive-disorder.jpg",
   },
   {
-    "Authors": "Gerald Roy Patterson",
-    "ISBN13": "978-0878221301",
-    "Title": "Living With Children: New Methods for Parents and Teachers",
-    "bookCover": "TBD"
+    Author: ["Gerald Roy Patterson"],
+    ISBN13: "978-0878221301",
+    Title: "Living With Children: New Methods for Parents and Teachers",
+    bookCover: "./img/book-covers/living-with-children-new-methods-for-parents-and-teachers.jpg",
   },
   {
-    "Authors": "John S. March, MD MPH; Karen Mulle Friesen ",
-    "ISBN13": "978-1572302426",
-    "Title":
+    Author: ["John S. March, MD MPH", "Karen Mulle Friesen"],
+    ISBN13: "978-1572302426",
+    Title:
       "OCD in Children and Adolescents: A Cognitive-Behavioral Treatment Manual",
-      "bookCover": "TBD"
+    bookCover: "./img/book-covers/ocd-in-children-and-adolescents-a-cognitive-behavioral-treatment-manual.jpg",
   },
   {
-    "Authors": "Aureen Pinto Wagner",
-    "ISBN13": "978-0967734736 ",
-    "Title": "Worried No More: Help and Hope for Anxious Children",
-    "bookCover": "TBD"
+    Author: ["Aureen Pinto Wagner"],
+    ISBN13: "978-0967734736",
+    Title: "Worried No More: Help and Hope for Anxious Children",
+    bookCover: "./img/book-covers/worried-no-more-help-and-hope-for-anxious-children.jpeg",
   },
   {
-    "Authors": "Rena Benson; Jose Arturo",
-    "ISBN13": "978-0963907059",
-    "Title": "You do that too? Adolescents and OCD",
-    "bookCover": "TBD"
+    Author: ["Rena Benson", "Jose Arturo"],
+    ISBN13: "978-0963907059",
+    Title: "You Do That Too? Adolescents and OCD",
+    bookCover: "./img/book-covers/you-do-that-too-adolescents-and-ocd.jpeg",
   },
   {
-    "Authors": "Rod Fadem",
-    "ISBN13": "978-0974290058",
-    "Title": "Memoirs of a Born Shlepper:  Never Give OCD a Third Thought",
-    "bookCover": "TBD"
+    Author: ["Rod Fadem"],
+    ISBN13: "978-0974290058",
+    Title: "Memoirs of a Born Shlepper: Never Give OCD a Third Thought",
+    bookCover: "./img/book-covers/memoirs-of-a-born-shlepper-never-give-ocd-a-third-thought.jpeg",
   },
 ];
