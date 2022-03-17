426bdc47c4be48a2b70973c90f052268e7473776
Brian Prost <brian.s.prost@gmail.com>
2022-03-17 13:22:08 -0700

    information section styling for mobile

--- components/Information.jsx
+++ components/Information.jsx
@@ -13,15 +13,18 @@ const InformationSection = () => {
         <div className="flex w-full items-center justify-evenly">
           <ul className="flex list-none flex-wrap justify-center">
             {HomepageLinks.map((HomepageLink) => (
-              <li className="m-4 flex p-2" key={HomepageLink.title}>
+              <li
+                className="my-2 flex py-2 lg:m-4 lg:p-2"
+                key={HomepageLink.title}
+              >
                 <a
-                  className="block max-w-sm transform rounded-lg border border-gray-100 bg-inherit p-6 px-8 shadow-md transition duration-500 ease-in-out hover:-translate-y-1 hover:bg-blue-500 hover:shadow-lg "
+                  className="block max-w-sm transform rounded-lg border border-gray-100 bg-inherit p-6 shadow-md transition duration-500 ease-in-out hover:-translate-y-1 hover:bg-blue-500 hover:shadow-lg lg:px-8 "
                   href={HomepageLink.link}
                 >
                   <h5 className="mb-6 text-center font-title text-2xl tracking-tight text-white lg:text-4xl">
                     {HomepageLink.title}
                   </h5>
-                  <p className="w-full text-center font-normal text-gray-100 lg:text-left">
+                  <p className="w-full text-center font-normal text-gray-100">
                     {HomepageLink.description}
                   </p>
                 </a>
