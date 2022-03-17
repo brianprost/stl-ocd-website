c9a951902c02405aa5f99ebd264baa2ef4a2a6c0
Brian Prost <brian.s.prost@gmail.com>
2022-03-17 11:53:10 -0700

    gave each article an id

--- data/Articles.js
+++ data/Articles.js
@@ -1,51 +1,60 @@
 export const Articles = [
   {
+    id: 1,
     link: "http://www.psychologytoday.com/blog/beyond-the-doubt",
     title: "Beyond the Doubt",
     description:
       "Psychology Today - An inside perspective on life with OCD and the lessons of uncertainty.",
   },
   {
+    id: 2,
     title: "20 Secret Signs of Addiction",
     link: "https://www.caring.com/articles/20-secret-signs-of-addiction",
     description: "",
   },
   {
+    id: 3,
     link: "http://stlocd.org/wp-content/uploads/2016/08/nimhanxiety.pdf",
     title: "Anxiety Disorders",
     description:
       "a Pamphlet from the National Institute of Mental Health that outlines the symptoms, research, and treatment of the various Anxiety Disorders: panic disorder, obsessivecompulsive disorder (OCD), posttraumatic stress disorder (PTSD), social phobia (or social anxiety disorder), specific phobias, and generalized anxiety disorder (GAD)",
   },
   {
+    id: 4,
     link: "http://stlocd.org/wp-content/uploads/2016/08/BodyDismorphicDisorder-PerceptionProblem.pdf",
     title: "Body Dysmorphic Disorder: A Problem of Perception?",
     description:
       "by Jamie Fuesner, M.D. from the Winter 2009 OCD Newsletter. This article outlines research into the symptoms and causes of Body Dysmorphic Disorder",
   },
   {
+    id: 5,
     link: "http://stlocd.org/wp-content/uploads/2016/08/Body_Dysmorphic_Disorder_Update_Spring_2006.pdf",
     title: "Body Dysmorphic Disorder: Update on A Spectrum Disorder",
     description:
       "by Elana Golan, from the Sring 2006 OCD Newsletter. This article updates what is known about BDD, How to determine if someone has BDD, treatment options and relates the personal story of the author's daughter",
   },
   {
+    id: 6,
     link: "http://stlocd.org/wp-content/uploads/2016/08/Clearing_Clutter_to_Create_Clarity.pdf",
     title: "Clearing Clutter to Create Clarity",
     description:
       "by DeniseLee, Professional Organizer, an article from the Java Journa",
   },
   {
+    id: 7,
     link: "http://stlocd.org/wp-content/uploads/2016/08/Clutter_Looking_at_the_Meaning_behind_the_Mess.pdf",
     title: "Clutter, Looking at the Meaning Behind the Mess",
     description: "by Mary Mohler, an article from Ladies Home Journa",
   },
   {
+    id: 8,
     link: "http://www.mssm.edu/research/centers/center-of-excellence-for-ocd/rating-scales",
     title: "CY-BOCS",
     description:
       "The complete Children's Yale-Brown Symptom Checklist and CY-BOCS Serverity Ratings Scale for both Obsessions and Compulsions. This is the test used by professional to evaluate the severity of OCD symptoms. This is a link to Mount Sinai School of Medicine download page to allow you to download the official copy of CY-BOCS. Please scroll to the bottom of the page",
   },
   {
+    id: 9,
     link: "http://stlocd.org/wp-content/uploads/2016/08/DeepBrainStimulation1-2.pdf",
     title:
       "Deep Brain Stimulation for Highly Treatment-Resistant OCD by Darin Dougherty, M.D. and Benjamin Greenberg, M.D., Ph.D.",
@@ -53,88 +62,103 @@ export const Articles = [
       "Discussion of alternative methods of OCD Treatment using both invasive (surgical) and non-invasive (radiation) brain procedures. Particular attention is given to Deep Brain Stimulation and the results to date",
   },
   {
+    id: 10,
     link: "http://stlocd.org/wp-content/uploads/2016/08/Deep_Brain_Stimulation_by_Jamie_Talan_review.pdf",
     title:
       "Deep Brain Stimulation, A New Treatment Shows Promise in the Most Difficult Cases, by Jamie Talan, reviewed by Jamie Feusner, MD.",
     description: "",
   },
   {
+    id: 11,
     link: "http://stlocd.org/wp-content/uploads/2016/08/Defeating_Obsessive_Thoughts_Larry_Kiel.pdf",
     title: "Defeating Obsessive Thoughts (On Your Own Terms):",
     description: "by Larry Kiel, presented March 2002",
   },
   {
+    id: 12,
     link: "http://stlocd.org/wp-content/uploads/2016/08/dont-try-harder-try-different.pdf",
     title: "Don't Try Harder, Try Different",
     description:
       "by Patrick McGrath, Ph.D. from the Fall 2008 OCD Newletter. Six ideas to incorporate in overcoming OCD",
   },
   {
+    id: 13,
     link: "http://stlocd.org/wp-content/uploads/2016/08/Expert_Concensus_Treatment_OCD.pdf",
     title:
       "Expert Concensus Treatment Guidelines for Obsessive-Compulsive Disorder: A Guide for Patients and Families",
     description: "Treatment Guidelines from Expert Knowledge Systems",
   },
   {
+    id: 14,
     link: "http://stlocd.org/wp-content/uploads/2016/08/Getting-the-Right-Treatment.pdf",
     title: "Getting the Right Treatment:",
     description:
       "A checklist of questions to ask your treatment provider to make sure they are qualified to treat you",
   },
   {
+    id: 15,
     link: "http://stlocd.org/wp-content/uploads/2016/08/Good_Coping_Gone_Bad_Gary_Mitchell.pdf",
     title: "Good Coping Gone Bad:",
     description:
       "When coping strategies interfere with progress in therapy by Gary Mitchell, MSW., ICSW. St. Louis Behavioural Medicine Institut",
   },
   {
+    id: 16,
     link: "http://stlocd.org/wp-content/uploads/2016/08/GuidelinesForLivingWithOCD.pdf",
     title: "Guidelines for Living with OCD:",
     description:
       "General Guidelines for family members living with a family member with OCD",
   },
   {
+    id: 17,
     link: "http://stlocd.org/wp-content/uploads/2016/08/Hoarding-Fact-Sheet.pdf",
     title: "Hoarding Fact Sheet OCF:",
     description:
       "A concise description of hoarding and how it differs from collecting. Difficulties Hoarders face in getting rid of things. Hoarding and OCD. Help for Hoarders. Treatment, medication and further information",
   },
   {
+    id: 18,
     link: "http://stlocd.org/wp-content/uploads/2016/08/I-Will-Never-Give-UP-Poem.pdf",
     title: "I Will Never Give Up",
     description: "Inspirational poem",
   },
   {
+    id: 19,
     link: "http://stlocd.org/wp-content/uploads/2016/08/InformationHoardingTheNeedtoKnowandRemember.pdf",
     title: "Information Hoarding: The Need to Know and Remember:",
     description:
       "By Renae M. Reinardy, Psy.D., LP, Lakeside Center for Behavioral Change, Minnetonka, M",
   },
   {
+    id: 20,
     link: "http://stlocd.org/wp-content/uploads/2016/08/IsYourChildWithOCDEligibleforSocialSecurityBenefits.pdf",
     title: "Is Your Child with OCD Eligible for Social Security Benefits?",
     description:
       "Eligibility for Supplemental Security Income or Social Security Disability Insurance, how to apply, and qualifications. Also information about Medicaid, Children with Special Needs, State Children's Health Insurance Programs and Employment Support Programs",
   },
   {
+    id: 21,
     link: "http://stlocd.org/wp-content/uploads/2016/08/LivingWithSomeoneWhoHas-OCD.pdf",
     title: "Living with Someone Who Has OCD: Guidelines for Family Members",
     description:
       "by Barbara Livingston Van Noppen, Ph.D. and Michele Tortora Pato, M.D. from the Spring 2009 OCD Newsletter. Keys to interacting with a family member with OCD",
   },
   {
+    id: 22,
     link: "http://stlocd.org/wp-content/uploads/2016/08/LivingWithYourWorstNightmare.pdf",
     title: "Living With Your Worst Nightmare:",
     description:
       "The Role of Mindfulness and Acceptance in Exposure and Response Preventio",
   },
   {
+    id: 23,
     link: "http://stlocd.org/wp-content/uploads/2016/08/Medication.pdf",
     title: "Medication:",
     description:
       "How medications are named, brand vs generic. Types of medications used for OCD and Depression. Side effects and their management. Medication management and safety. Checklist to take to your doctor to make sure you get all the information about your medications",
   },
   {
+    id: 24,
     link: "http://stlocd.org/wp-content/uploads/2016/08/NewOCDResearchonGlutamate.pdf",
     title:
       "New Horizons in OCD Research and the Potential Importance of Glutamate: Can We Develop Treatments That Work Better and Faster?",
@@ -142,85 +166,100 @@ export const Articles = [
       "by Michael H. Block, M.D., Vladimir Coric, M.D., and Christopher Pittenger, M.D. Ph.D. from the Spring 2009 OCD Newsletter. A promising new treatment for OCD",
   },
   {
+    id: 25,
     link: "http://stlocd.org/wp-content/uploads/2016/08/NIMH_Facts_About_Anxiety_Disorders.pdf",
     title: "NIMH Anxiety Disorders",
     description:
       "A brief summary about the various types of Anxiety Disorders. Includes a short quiz to test your understanding",
   },
   {
+    id: 26,
     link: "http://stlocd.org/wp-content/uploads/2016/08/OCF-Donation.pdf",
     title: "Obsessive Compulsive Foundation (OCF) Contribution Form",
     description: "",
   },
   {
+    id: 27,
     link: "http://stlocd.org/wp-content/uploads/2016/08/OCD-and-Social-Security-Disability-Benefits.pdf",
     title: "OCD and Social Security Disability Benefits",
     description:
       "Discusses Social Security Benefit Programs, Medical Eligibility, Application Preparation and Applying and Receiving a decision",
   },
   {
+    id: 28,
     link: "http://stlocd.org/wp-content/uploads/2016/08/JAN_OCD_and_the_Workplace.pdf",
     title: "OCD and the Workplace",
     description:
       "The Job Accomodation Network (JAN) explains how the Americans with Disabilities Act (ADA) can help individuals get and keep jobs",
   },
   {
+    id: 29,
     link: "http://stlocd.org/wp-content/uploads/2016/08/OCD_For_Dummies_Review.pdf",
     title: "OCD For Dummies",
     description:
       "by Charles Elliot, PHd and Larua Smith, PHDA book review by Deb Osgood-Hynes, Psy",
   },
   {
+    id: 30,
     link: "http://stlocd.org/wp-content/uploads/2016/08/New-OCD-Guide-for-College.pdf",
     title: "OCD Guide for College, Fall 2008",
     description: "Overcoming OCD A Guide for College Student",
   },
   {
+    id: 31,
     link: "http://stlocd.org/wp-content/uploads/2016/08/OCD-Facts-NIMH.pdf",
     title: "OCD Fact Sheet from the National Institute of Mental Health:",
     description:
       "General information on OCD. How common is OCD. What causes OCD. Treatments available",
   },
   {
+    id: 32,
     link: "http://stlocd.org/wp-content/uploads/2016/08/IOCDF_Launches_New_Website.pdf",
     title: "OCD International OCD Foundation Launches New Web Site",
     description:
       "New, improved and greatly expanded. Check out the new features and articles",
   },
   {
+    id: 33,
     link: "http://stlocd.org/wp-content/uploads/2016/08/OCD-Providers-STL.pdf",
     title: "OCD Providers:",
     description: "A partial list of provider of treatment for OCD",
   },
   {
+    id: 34,
     link: "http://stlocd.org/wp-content/uploads/2016/08/OCD_Resources.pdf",
     title: "OCD Resources:",
     description:
       "A list of resources available to obtain free or low cost medications, therapy and support",
   },
   {
+    id: 35,
     link: "http://stlocd.org/wp-content/uploads/2016/08/OCD_Survival_Guide.pdf",
     title: "OCD Survival Guide:",
     description:
       "A guide to help the family and friends of someone with OCD. This book is generally available",
   },
   {
+    id: 36,
     link: "http://stlocd.org/wp-content/uploads/2016/08/OCD-Treatment.pdf",
     title: "OCD Treatment:",
     description: "Prognosis, Primary Medications, Secondary Medication",
   },
   {
+    id: 37,
     link: "http://stlocd.org/wp-content/uploads/2016/08/OCD-Tribe.pdf",
     title: "OCD Tribe:",
     description: "An Online Support Community for those with OCD of all age",
   },
   {
+    id: 38,
     link: "http://stlocd.org/wp-content/uploads/2016/08/Pandas_Autoimmune.pdf",
     title: "PANDAS: A Model for Autoimmune Neuropsychiatric Disorders",
     description:
       "from Primary Psychiatry, written by Susan E. Swedo, MD, and Paul J. Grant, M",
   },
   {
+    id: 39,
     link: "http://stlocd.org/wp-content/uploads/2016/08/PANDAS-Fact-Sheet.pdf",
     title:
       "PANDAS Pediatric Autoimmune Neuropsychiatric Disorder Associated with Streptococcus:",
@@ -228,12 +267,14 @@ export const Articles = [
       "A fact sheet written by Evelyn Stewart, MD, Massachusetts General Hospital. Diagnosis and Treatment",
   },
   {
+    id: 40,
     link: "http://stlocd.org/wp-content/uploads/2016/08/Self-DirectedTreatmentforOCD.pdf",
     title: "Self-Directed Treatment for OCD:The Irony of Doing the Opposite",
     description:
       " by Paul R. Munford, Ph.D. Exposure, Ritual Prevention, and Awareness Exercises",
   },
   {
+    id: 41,
     link: "http://stlocd.org/wp-content/uploads/2016/08/Someone_I_Care_About_OCD_What_Can_I_Do.pdf",
     title:
       "Someone I Care About Is Not Dealing With His OCD: What Can I Do About It?",
@@ -241,63 +282,74 @@ export const Articles = [
       "By Heidi J. Pollard, RN, MSN and C. Alec Pollard, Ph.D., Anxiety Disorders Center, Saint Louis Behavioral Medicine Institute and Saint Louis University",
   },
   {
+    id: 42,
     link: "http://stlocd.org/wp-content/uploads/2016/08/Suggested-Reading-for-the-Family.pdf",
     title: "Suggested Reading for the Family",
     description: "",
   },
   {
+    id: 43,
     link: "http://stlocd.org/wp-content/uploads/2016/08/12-Step-Prog-OCD-Recovery-OCA-12-Step-Order-Form.pdf",
     title: "The 12 Step Program and OCD Recovery",
     description:
       "Provides information on the OCA (Obsessive Compulsive Association) the their 12 Step Program to recovery",
   },
   {
+    id: 44,
     link: "http://stlocd.org/wp-content/uploads/2016/08/ReviewofOCDAnswerBook.pdf",
     title:
       "The OCD Answer Book by Patrick McGrath, Ph.D. reviewed by Eric Storch, Ph.D.",
     description: "from the Winter 2009 OCD Newsletter",
   },
   {
+    id: 45,
     link: "http://stlocd.org/wp-content/uploads/2016/08/RelationshipEating-Disorders-OCD-1-3.pdf",
     title:
       "The Relationship Between Eating Disorders and OCD: Part of the Spectrum",
     description: " by Fugen Neziroglu, Ph.D., ABBP and Jonathan Sandler, B",
   },
   {
+    id: 46,
     link: "http://stlocd.org/wp-content/uploads/2016/08/TheThingInsideMyHead.pdf",
     title:
       "The Things Inside My Head: A Family's Journey through Mental Illness",
     description: "by Lois Chamber, reviewed by S. Evelyn Stewart, M.D",
   },
   {
+    id: 47,
     link: "http://stlocd.org/wp-content/uploads/2016/08/Things-You-can-do-to-prepare-for-Treatment.pdf",
     title: "Things You can do to prepare for Treatment",
     description:
       "A checklist of items to review and perform before starting treatment for OCD. Prepared by C. Alec Pollard, Ph.D",
   },
   {
+    id: 48,
     link: "http://stlocd.org/wp-content/uploads/2016/08/This-I-Do-For-Me.pdf",
     title: "This I Do For Me",
     description: "by Karen M. Curran from October 1995 OCD Newslette",
   },
   {
+    id: 49,
     link: "http://stlocd.org/wp-content/uploads/2019/11/Understanding-Hoarding.pdf",
     title: "Understanding Hoarding",
     description:
       "by Debbie Kricensky, Resource Coordinator Problem Properties Unit, St. Louis County Public Work",
   },
   {
+    id: 50,
     link: "http://stlocd.org/wp-content/uploads/2016/08/WhatMightBeCausingOCDSymptoms.pdf",
     title: "What Might be Causing OCD Symptoms?",
     description:
       "By Suk Won Kim, MD, University of Minnesota Medical School. Brain function and disfunction in OCD patients",
   },
   {
+    id: 51,
     link: "http://stlocd.org/wp-content/uploads/2016/08/Why_Am_I_Not_Getting_Better.pdf",
     title: "Why Am I Not Getting Better?",
     description: "by Laurie Krauth, MA, LL",
   },
   {
+    id: 52,
     link: "http://www.mssm.edu/research/centers/center-of-excellence-for-ocd/rating-scales",
     title: "Y-BOCS Symptom Checklist",
     description:
