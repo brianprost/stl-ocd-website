d816fc8989c4e3d4e274d09605a0c997cd713a44
Brian Prost <brian.s.prost@gmail.com>
2022-03-17 12:43:33 -0700

    landing page widening for mobile devices

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -3,7 +3,7 @@ const Landing = () => {
 
   return (
     <section className="bg-white-100 flex justify-center pb-12" id="home">
-      <div className="flex items-center justify-center px-8 pt-20 lg:pt-28 lg:pb-16 ">
+      <div className="flex items-center justify-center sm:px-8 pt-20 lg:pt-28 lg:pb-16 ">
         <div className="w-5/6 md:w-1/2 ">
           <h1 className="md:leading-2 py-16 text-center font-title text-7xl tracking-wide text-gray-900 md:pb-0 md:text-8xl lg:text-7xl xl:text-8xl ">
             Helping Hands for Those With
@@ -11,7 +11,7 @@ const Landing = () => {
           </h1>
           <a href={latestFlyerLink}>
             <div className="lg:justify-left flex items-center justify-center">
-              <div className="m-10 w-3/5 transform rounded-xl bg-blue-800 p-2 text-center text-2xl font-normal text-white  shadow-lg transition duration-500 ease-in-out hover:-translate-y-1 hover:bg-gray-900 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-800 focus:ring-offset-4">
+              <div className="m-10 w-full lg:w-3/5 transform rounded-xl bg-blue-800 p-2 text-center text-2xl font-normal text-white  shadow-lg transition duration-500 ease-in-out hover:-translate-y-1 hover:bg-gray-900 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-800 focus:ring-offset-4">
                 📆 Upcoming Events
               </div>
             </div>
