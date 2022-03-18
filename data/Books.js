8842943e4ba900e35fe7e454e3b564c171312349
Brian Prost <brian.s.prost@gmail.com>
2022-03-18 13:23:10 -0700

    converted images to webp

--- data/Books.js
+++ data/Books.js
@@ -3,14 +3,14 @@ export const Books = [
     Author: ["E. Katia Moritz", "Jennifer Jablonsky"],
     ISBN13: "978-1882732722",
     Title: "Blink, Blink, Clop, Clop: An OCD Storybook",
-    bookCover: "./img/book-covers/blink-blink-clop-clop-an-ocd-storybook.jpeg",
+    bookCover: "./img/book-covers/blink-blink-clop-clop-an-ocd-storybook.webp",
   },
   {
     Author: ["Tamar E. Chansky"],
     ISBN13: "978-0812931174",
     Title: "Freeing Your Child from Obsessive-Compulsive Disorder",
     bookCover:
-      "./img/book-covers/freeing-your-child-from-obsessive-compulsive-disorder.jpeg",
+      "./img/book-covers/freeing-your-child-from-obsessive-compulsive-disorder.webp",
   },
   {
     Author: ["Constance H. Foster"],
@@ -18,14 +18,14 @@ export const Books = [
     Title:
       "Kids Like Me: Children's Stories About Obsessive Compulsive Disorder",
     bookCover:
-      "./img/book-covers/kids-like-me-children-s-stories-about-obsessive-compulsive-disorder.jpg",
+      "./img/book-covers/kids-like-me-children-s-stories-about-obsessive-compulsive-disorder.webp",
   },
   {
     Author: ["Gerald Roy Patterson"],
     ISBN13: "978-0878221301",
     Title: "Living With Children: New Methods for Parents and Teachers",
     bookCover:
-      "./img/book-covers/living-with-children-new-methods-for-parents-and-teachers.jpg",
+      "./img/book-covers/living-with-children-new-methods-for-parents-and-teachers.webp",
   },
   {
     Author: ["John S. March, MD MPH", "Karen Mulle Friesen"],
@@ -33,26 +33,26 @@ export const Books = [
     Title:
       "OCD in Children and Adolescents: A Cognitive-Behavioral Treatment Manual",
     bookCover:
-      "./img/book-covers/ocd-in-children-and-adolescents-a-cognitive-behavioral-treatment-manual.jpg",
+      "./img/book-covers/ocd-in-children-and-adolescents-a-cognitive-behavioral-treatment-manual.webp",
   },
   {
     Author: ["Aureen Pinto Wagner"],
     ISBN13: "978-0967734736",
     Title: "Worried No More: Help and Hope for Anxious Children",
     bookCover:
-      "./img/book-covers/worried-no-more-help-and-hope-for-anxious-children.jpeg",
+      "./img/book-covers/worried-no-more-help-and-hope-for-anxious-children.webp",
   },
   {
     Author: ["Rena Benson", "Jose Arturo"],
     ISBN13: "978-0963907059",
     Title: "You Do That Too? Adolescents and OCD",
-    bookCover: "./img/book-covers/you-do-that-too-adolescents-and-ocd.jpeg",
+    bookCover: "./img/book-covers/you-do-that-too-adolescents-and-ocd.webp",
   },
   {
     Author: ["Rod Fadem"],
     ISBN13: "978-0974290058",
     Title: "Memoirs of a Born Shlepper: Never Give OCD a Third Thought",
     bookCover:
-      "./img/book-covers/memoirs-of-a-born-shlepper-never-give-ocd-a-third-thought.jpeg",
+      "./img/book-covers/memoirs-of-a-born-shlepper-never-give-ocd-a-third-thought.webp",
   },
 ];
