4e75edc32d07f7730b4aafc0be408a7d88c90a07
Brian Prost <brian.s.prost@gmail.com>
2022-03-17 17:47:30 -0700

    destructured book props

--- pages/books/index.jsx
+++ pages/books/index.jsx
@@ -13,8 +13,14 @@ const BooksPage = () => {
           Books
         </h1>
         <div className="mx-auto grid px-10 md:grid-cols-2 lg:grid-cols-3 lg:gap-10 lg:space-y-0 lg:px-0 xl:grid-cols-4">
-          {Books.map((book) => (
-            <Book book={book} key={book.Title} />
+          {Books.map(({ Author, ISBN13, Title, bookCover }) => (
+            <Book
+              Author={Author}
+              ISBN13={ISBN13}
+              Title={Title}
+              bookCover={bookCover}
+              key={ISBN13}
+            />
           ))}
         </div>
       </div>
