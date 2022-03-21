5f7c881d1e255756e4ac81146801f3042d3875e4
Brian Prost <brian.s.prost@gmail.com>
2022-03-21 12:36:42 -0700

    fixed favicons

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -21,9 +21,94 @@ class MyDocument extends Document {
             content="The digital home for the St. Louis OCD Support Group."
           />
 
+          <meta
+            name="theme-color"
+            content="rgb(59 130 246)"
+            media="(prefers-color-scheme: light)"
+          />
+          <meta
+            name="theme-color"
+            content="rgb(30 58 138)"
+            media="(prefers-color-scheme: dark)"
+          />
+
           {/* FAVICONS */}
           {/* TODO: fix favicon  */}
-          <link rel="shortcut icon" href="/favicon.svg" />
+          {/* For old IEs */}
+          <link rel="shortcut icon" href="/img/favicon/favicon.ico" />
+
+          {/* For new browsers - multisize ico  */}
+          <link
+            rel="icon"
+            type="image/x-icon"
+            sizes="16x16 32x32"
+            href="/img/favicon/favicon.ico"
+          />
+
+          {/* For iPad with high-resolution Retina display running iOS ≥ 7: */}
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/img/favicon/favicon-152-precomposed.png"
+          />
+
+          {/* For iPad with high-resolution Retina display running iOS ≤ 6: */}
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/img/favicon/favicon-144-precomposed.png"
+          />
+
+          {/* For iPhone with high-resolution Retina display running iOS ≥ 7: */}
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/img/favicon/favicon-120-precomposed.png"
+          />
+
+          {/* For iPhone with high-resolution Retina display running iOS ≤ 6: */}
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/img/favicon/favicon-114-precomposed.png"
+          />
+
+          {/* For iPhone 6+ */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/img/favicon/favicon-180-precomposed.png"
+          />
+
+          {/* For first- and second-generation iPad: */}
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/img/favicon/favicon-72-precomposed.png"
+          />
+
+          {/* For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: */}
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="/img/favicon/favicon-57.png"
+          />
+
+          {/* For Old Chrome */}
+          <link rel="icon" sizes="32x32" href="/img/favicon/favicon-32.png" />
+
+          {/* For IE10 Metro */}
+          <meta name="msapplication-TileColor" content="#FFFFFF" />
+          <meta name="msapplication-TileImage" content="favicon-144.png" />
+          <meta name="theme-color" content="#ffffff" />
+
+          {/* Chrome for Android */}
+          <link rel="manifest" href="/img/favicon/manifest.json" />
+          <link
+            rel="icon"
+            sizes="192x192"
+            href="/img/favicon/favicon-192.png"
+          ></link>
         </Head>
         <body>
           <Main />
