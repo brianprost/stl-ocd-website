46c244ee1baf9556bc56d1cb09339a58cd881a0f
Brian Prost <brian.s.prost@gmail.com>
2022-12-03 15:17:17 -0500

    books page now loads from firebase

--- pages/books/index.jsx
+++ pages/books/index.jsx
@@ -1,8 +1,15 @@
 import Head from "next/head";
 import Book from "../../components/Book.component";
-import { Books } from "../../data/Books";
+import { ref, getDatabase } from "firebase/database";
+import LoadingSpinner from "../../components/LoadingSpinner";
+import firebaseApp from "../../firebase";
+import { useList } from "react-firebase-hooks/database";
+
+const database = getDatabase(firebaseApp);
 
 const BooksPage = () => {
+  const [snapshots, loading, error] = useList(ref(database, "books"));
+
   return (
     <>
       <Head>
@@ -12,14 +19,20 @@ const BooksPage = () => {
         <h1 className="my-10 text-center font-title text-6xl text-blue-800 drop-shadow-md lg:text-7xl">
           Books
         </h1>
+        {error && <strong>Error: {error}</strong>}
+        {loading && (
+          <div className="flex h-screen flex-row items-center justify-center md:h-3/4">
+            <LoadingSpinner />
+          </div>
+        )}
         <div className="mx-auto grid px-10 md:grid-cols-2 lg:grid-cols-3 lg:gap-10 lg:space-y-0 lg:px-0 xl:grid-cols-4">
-          {Books.map(({ Author, ISBN13, Title, bookCover }) => (
+          {snapshots.map((book) => (
             <Book
-              Author={Author}
-              ISBN13={ISBN13}
-              Title={Title}
-              bookCover={bookCover}
-              key={ISBN13}
+              Author={book.val().Author}
+              ISBN13={book.val().ISBN13}
+              Title={book.val().Title}
+              bookCover={book.val().bookCover}
+              key={book.val().ISBN13}
             />
           ))}
         </div>
