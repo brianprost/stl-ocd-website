9d3dddf2b670f66ca708cf308556ce02e20fffe0
Brian Prost <me@brianprost.com>
2024-09-07 11:55:33 -0400

    edge runtime for cloudflare deployment

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+export const runtime = 'edge';
+
 export default function helloAPI(req, res) {
   // why the heck would anyone request an API endpoint on this site?
   // whatever, give them our name
